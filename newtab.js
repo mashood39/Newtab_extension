256f6195749ba186b1c1b45ffbf792bdd82b66c0
masho <mashoodck77@gmail.com>
2025-09-15 00:35:56 +0530

    list style udpated

--- newtab.js
+++ newtab.js
@@ -4,13 +4,18 @@ function loadBookmarks() {
   list.innerHTML = ""
 
   chrome.bookmarks.getTree((nodes) => {
+    console.log(nodes)
     function traverse(bookmarkNodes) {
       for (let node of bookmarkNodes) {
         if(node.url) {
           const li = document.createElement('li');
           const a = document.createElement('a');
           a.href = node.url;
-          a.textContent = node.title || node.url;
+          let text = node.title || node.url;
+          if(text.length > 30) {
+            text = text.substring(0,30) + "...";
+          }
+          a.textContent = text;
           a.target = "_blank"
           li.appendChild(a);
           list.appendChild(li);
